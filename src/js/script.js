1b2f10abf438090c4a1817e3812a0af31236da83
Monika Wieczorek <monikawieczoreq@gmail.com>
2019-11-23 12:32:45 +0100

    Add products

--- src/js/script.js
+++ src/js/script.js
@@ -53,16 +53,43 @@
   };
 
   class Product{
-    constructor(){
+    constructor(id, data){
       const thisProduct = this;
-
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
       console.log('new Product: ', thisProduct);
     }
+    renderInMenu(){
+      const thisProduct = this;
+
+      /*generate HTML based on template*/
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      console.log('generatedHTML: ', generatedHTML);
+
+      /*create element using utils.createElementFromHTML*/
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /*find menu container*/
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      
+      /*add element to menu*/
+      menuContainer.appendChild(thisProduct.element);
+    }
   }
+
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct: ', testProduct);
+      const thisApp = this;
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = dataSource;
+      console.log('thisApp.data: ', thisApp.data);
     },
 
     init: function(){
@@ -73,6 +100,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
