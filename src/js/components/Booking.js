8ecc8f027f00ad03389cb77b870f45640e5be04a
Monika Wieczorek <monikawieczoreq@gmail.com>
2019-12-08 18:47:34 +0100

    Add class Booking

---
+++ src/js/components/Booking.js
@@ -0,0 +1,33 @@
+import utils from '../utils.js';
+import {templates, select} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking{
+  constructor(bookingWrapper){
+    const thisBooking = this;
+
+    thisBooking.render(bookingWrapper);
+    thisBooking.initWidgets();
+  }
+
+  render(bookingWrapper){
+    const thisBooking = this;
+
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper  = bookingWrapper;
+    thisBooking.dom.wrapper = utils.createDOMFromHTML(generatedHTML);
+    bookingWrapper.appendChild(thisBooking.dom.wrapper);
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+  }
+
+  initWidgets(){
+    const thisBooking = this;
+
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+  }
+}
+
+export default Booking;
