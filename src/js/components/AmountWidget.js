9659e2b6c7297dd5af20f2d77b74f6a70aeb1c51
Monika Wieczorek <monikawieczoreq@gmail.com>
2019-12-09 08:16:37 +0100

    Add get value Add set value

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -9,7 +9,7 @@ class AmountWidget extends BaseWidget{
     //thisWidget.value = settings.amountWidget.defaultValue;
     //thisWidget.setValue(thisWidget.input.value);
     thisWidget.initActions();
-    console.log('AmountWidget: ', thisWidget);
+    //console.log('AmountWidget: ', thisWidget);
     //console.log('constructor arguments: ', element);
   }
 
@@ -38,7 +38,8 @@ class AmountWidget extends BaseWidget{
     const thisWidget = this;
 
     thisWidget.dom.input.addEventListener('change', function(){
-      thisWidget.setValue(thisWidget.dom.input.value);
+      //thisWidget.setValue(thisWidget.dom.input.value);
+      thisWidget.value = thisWidget.dom.input.value;
     });
 
     thisWidget.dom.linkDecrease.addEventListener('click', function(){
