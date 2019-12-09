99bb99da14cd22e20fbcec8d326918dd7b812d20
Monika Wieczorek <monikawieczoreq@gmail.com>
2019-12-09 13:03:49 +0100

    Start class DatePicker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,7 @@
 import utils from '../utils.js';
 import {templates, select} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
 
 class Booking{
   constructor(bookingWrapper){
@@ -20,6 +21,7 @@ class Booking{
     bookingWrapper.appendChild(thisBooking.dom.wrapper);
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
   }
 
   initWidgets(){
@@ -27,6 +29,7 @@ class Booking{
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
   }
 }
 
