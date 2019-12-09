d51f5837e66ce2eca2e30aad5353c977dd569137
Monika Wieczorek <monikawieczoreq@gmail.com>
2019-12-09 09:13:33 +0100

    Fix constructor in BaseWidget

--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -5,7 +5,7 @@ class BaseWidget{
     thisWidget.dom = {};
     thisWidget.dom.wrapper = wrapperElement;
 
-    thisWidget.correctValue = initialValue;
+    thisWidget.value = initialValue;
   }
   get value(){
     const thisWidget = this;
